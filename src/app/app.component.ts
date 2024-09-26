bec2bebfe09ffcfa8319a6a54c5a8d96827e734c
mylcevaleksandr <myltsevaleksandr@gmail.com>
2024-09-26 21:41:22 +0300

    Remade repo to keep track of my LeetCode and Codewars task solving progress.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LeetCode_Codewars_Tasks';
+
+  constructor() {
+    console.log(this.twoSum(this.numsArr, this.target));
+  }
+
+  private numsArr = [2, 5, 7, 9, 13];
+  private target = 9;
+
+  private twoSum(nums: number[], target: number): number[] | null {
+    const numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      const complement: number = target - nums[i];
+      if (numMap.has(complement)) {
+        return [numMap.get(complement), i];
+      }
+
+      numMap.set(nums[i], i);
+    }
+
+    return null;
+  }
 }
