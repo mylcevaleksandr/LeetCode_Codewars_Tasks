0c499baac4396a2481eb9575bd4487deeaf1e6f3
mylcevaleksandr <myltsevaleksandr@gmail.com>
2024-10-01 22:51:36 +0300

    Ran build from inside app directory

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
-import {debounceTime, filter} from "rxjs";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-header',
@@ -12,17 +12,17 @@ export class HeaderComponent {
   activeLink: number;
 
   constructor(private router: Router, private route: ActivatedRoute) {
-    this.activeLink = 5
+    this.activeLink = 5;
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      debounceTime(500)
+      filter(event => event instanceof NavigationEnd)
+      // debounceTime(500)
     ).subscribe(() => {
       const currentPath = this.route.snapshot.firstChild?.routeConfig?.path;
       this.activeLink = this.links.findIndex(link => link.ip === `/${currentPath}`);
-
-      if (this.activeLink < 0) {
-        this.router.navigate(["/home"]);
-      }
+      //
+      // if (this.activeLink < 0) {
+      //   this.router.navigate(["/home"]);
+      // }
     });
   }
 }
